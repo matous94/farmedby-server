fd209dcbd4829afc91b8664355b6fef0f3ad3dd9
Matouš Vencl <matousvencl@gmail.com>
2020-10-31 20:48:34 +0100

    add eslint and prettier

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-const ParseServer = require("parse-server").ParseServer;
+const path = require("path");
+const { ParseServer } = require("parse-server");
+
 const app = express();
 
 const api = new ParseServer({
   databaseURI: `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOSTNAME}?retryWrites=true&w=majority`, // Connection string for your MongoDB database
-  cloud: __dirname + "/cloud.js", // Absolute path to your Cloud Code
+  cloud: path.join(__dirname, "cloud.js"), // Absolute path to your Cloud Code
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY, // Keep this key secret!
   // fileKey: "optionalFileKey",
-  serverURL: `${process.env.SERVER_URL}/parse`, // Don't forget to change to https if needed
+  serverURL: `${process.env.SERVER_URL}/parse` // Don't forget to change to https if needed
 });
 
 // Serve the Parse API on the /parse URL prefix
 app.use("/parse", api);
 
-app.listen(process.env.PORT, function() {
-  console.log(`parse-server running on port ${process.env.PORT}.`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}.`);
 });
