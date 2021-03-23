ef0e754a358460526b9ffb4a60311cf0904ed284
Matouš Vencl <matousvencl@gmail.com>
2021-03-23 14:39:46 +0100

    add subscribeToNewsletter cloud function

--- cloud-functions.js
+++ cloud-functions.js
@@ -1,3 +1,16 @@
 require("./packages/farm/farm-functions");
 require("./packages/pickup-point/pickup-point-functions");
 require("./packages/box/box-functions");
+
+Parse.Cloud.define("subscribeToNewsletter", async ({ params }) => {
+  const { email } = params;
+  const isSubscribed = Boolean(
+    await new Parse.Query("Newsletter")
+      .equalTo("email", email)
+      .count({ useMasterKey: true })
+  );
+
+  if (isSubscribed) return;
+
+  await new Parse.Object("Newsletter").save({ email }, { useMasterKey: true });
+});
