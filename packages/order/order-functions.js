c82faec186dfa5334af64c56a204cd102e87a953
Matouš Vencl <matousvencl@gmail.com>
2021-05-12 13:30:59 +0200

    fix

--- packages/order/order-functions.js
+++ packages/order/order-functions.js
@@ -1,13 +1,34 @@
 Parse.Cloud.define("createOrder", async ({ params }) => {
   const order = new Parse.Object("Order", {
     completed: false,
+    customer: params.customer,
     farm: params.farm,
-    farmId: params.objectId,
+    farmId: params.farm.objectId,
     journal: "",
     note: params.note,
     pickupPoint: params.pickupPoint,
     subscriptions: params.subscriptions
   });
   await order.save(null, { useMasterKey: true });
+  return order.toJSON();
+});
+
+Parse.Cloud.define("getOrder", async ({ user, params }) => {
+  const orderQuery = new Parse.Query("Order");
+
+  let isOwner = false;
+  if (user) {
+    const count = await new Parse.Query("Farm")
+      .equalTo("owner", user)
+      .count({ useMasterKey: true });
+    isOwner = count > 0;
+  }
+  if (!isOwner) {
+    orderQuery.exclude("journal");
+  }
+
+  const order = await orderQuery.get(params.orderId, {
+    useMasterKey: true
+  });
   return order.toJSON();
 });
