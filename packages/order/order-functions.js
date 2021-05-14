a596853997f3598e46c8bbdf49904b3a56b8e783
Matouš Vencl <matousvencl@gmail.com>
2021-05-14 13:50:21 +0200

    add updateOrder cloud function

--- packages/order/order-functions.js
+++ packages/order/order-functions.js
@@ -14,21 +14,51 @@ Parse.Cloud.define("createOrder", async ({ params }) => {
 });
 
 Parse.Cloud.define("getOrder", async ({ user, params }) => {
-  const orderQuery = new Parse.Query("Order");
+  const order = await new Parse.Query("Order").get(params.orderId, {
+    useMasterKey: true
+  });
+  const orderAsJson = order.toJSON();
 
   let isOwner = false;
   if (user) {
     const count = await new Parse.Query("Farm")
       .equalTo("owner", user)
+      .equalTo("objectId", order.farmId)
       .count({ useMasterKey: true });
     isOwner = count > 0;
   }
-  if (!isOwner) {
-    orderQuery.exclude("journal");
-  }
 
-  const order = await orderQuery.get(params.orderId, {
-    useMasterKey: true
-  });
+  if (!isOwner) delete orderAsJson.journal;
+
   return order.toJSON();
 });
+
+Parse.Cloud.define(
+  "updateOrder",
+  async ({ user, params }) => {
+    const order = await new Parse.Query("Order")
+      .select("farmId")
+      .get(params.objectId, {
+        useMasterKey: true
+      });
+
+    let isOwner = false;
+    const count = await new Parse.Query("Farm")
+      .equalTo("owner", user)
+      .equalTo("objectId", order.get("farmId"))
+      .count({ useMasterKey: true });
+    isOwner = count > 0;
+    if (!isOwner) throw new Error("Not authorized");
+
+    await order.save(
+      {
+        journal: params.journal,
+        completed: params.completed
+      },
+      { useMasterKey: true }
+    );
+  },
+  {
+    requireUser: true
+  }
+);
